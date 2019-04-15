08d139b334b3fea343b23e24b11d8bb7626b7719
RicardoImagure <ricardo.imagure@gmail.com>
2019-04-15 11:14:23 -0300

    Pequena modificação na chamada .run()

--- app.py
+++ app.py
@@ -80,4 +80,5 @@ api.add_resource(PersonalAssistantEnglish, "/personal_assistant")
 
 port = int(os.environ.get('PORT', 5000))
 
-app.run(debug=True, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=port)
