e4537c40443980de49d7ea3444793d5d5356c01d
RicardoImagure <ricardo.imagure@gmail.com>
2019-04-29 11:48:31 -0300

    Adiciona mais alguns exemplos de fluxo no ontology_interface_test

--- ontology_interface_test.py
+++ ontology_interface_test.py
@@ -2,6 +2,8 @@ import rdflib
 from db.Ontology.ontology_interface import *
 
 sm_ontology = "db/Ontology/assistant.owl"
+text = "Ricardo"
+text2 = "Imagure"
 
 graph = rdflib.Graph()
 
@@ -15,8 +17,32 @@ contatos = query_for_object_property(graph, pessoa[0], "contato")
 
 print("object property: ", contatos)
 
+
 nomes_contatos = []
+sobrenomes_contatos = []
+contatos_encontrados = []
+pessoa_encontrada = []
 for pessoa in contatos:
-    nome = query_for_data_property(graph, pessoa, "Nome")
-    nomes_contatos.append(nome)
+    nomes = query_for_data_property(graph, pessoa, "Nome")
+    if text in nomes:
+        contatos_encontrados.append(pessoa)
+    nomes_contatos.append(nomes)
 print(nomes_contatos)
+if len(contatos_encontrados) > 1:
+    # if is_compound(text):
+    #   try_disambiguate(text)
+    for pessoa in contatos_encontrados:
+        sobrenomes = query_for_data_property(graph, pessoa, "Sobrenome")
+        if text2 in sobrenomes:
+            pessoa_encontrada.append(pessoa)
+    # else:
+    #   add_conflict(text)
+
+# if len(pessoa_encontrada) > 1:
+    # add_conflict()
+# else:
+print(pessoa_encontrada)
+
+id_contato = query_for_data_property(graph, pessoa_encontrada[0], "id")
+
+print(id_contato)
