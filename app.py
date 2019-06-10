9fd4219971f66e075b5867129c7aae7f2fe1363c
RicardoImagure <ricardo.imagure@gmail.com>
2019-06-10 11:38:01 -0300

    Modifica o Flask server com auth do slack

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ import os
 
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
-
+from slackclient import SlackClient
 from semanticizer.SemanticizerWorker import SemanticizerWorker
+
+# client_id = os.environ["client_id"]
+# client_secret = os.environ["client_secret"]
+# oauth_scope = os.environ["scope"]
+
+client_id = "594442784566.594081769079"
+client_secret = "b1c6ea964deecdf37068ce2bf6cb1977"
+oauth_scope = "bot,commands,chat:write:bot"
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,37 +22,84 @@ class PersonalAssistant(Resource):
     semanticizer = SemanticizerWorker()
     semanticizer.start()
 
-    @staticmethod
-    def get():
-
+    def get(self):
+        rule = request.url_rule
+        if "begin_auth" in rule.rule:
+            return self.pre_install()
+        elif "finish_auth" in rule.rule:
+            return self.post_install()
         return "Flask server up and running!", 200
+
+    @staticmethod
+    def pre_install():
+        return '''
+          <a href="https://slack.com/oauth/authorize?scope={0}&client_id={1}">
+              Add to Slack
+          </a>
+      '''.format(oauth_scope, client_id)
+
+    @staticmethod
+    def post_install():
+
+        # Retrieve the auth code from the request params
+        auth_code = request.args['code']
+        print("Auth_code: ", auth_code)
+
+        # An empty string is a valid token for this request
+        sc = SlackClient("")
+
+        # Request the auth tokens from Slack
+        auth_response = sc.api_call(
+            "oauth.access",
+            client_id=client_id,
+            client_secret=client_secret,
+            code=auth_code
+        )
+
+        os.environ["SLACK_USER_TOKEN"] = auth_response['access_token']
+        os.environ["SLACK_BOT_TOKEN"] = auth_response['bot']['bot_access_token']
+
+        slack_user_token = auth_response['access_token']
+        slack_bot_token = auth_response['bot']['bot_access_token']
+
+        print("slack user: ", slack_user_token)
+        print("slack bot: ", slack_bot_token)
+
+        # Don't forget to let the user know that auth has succeeded!
+        return "Auth complete!"
 
     def post(self):
 
-        self._set_semanticizer_language()
+        rule = request.url_rule
+        if "finish_auth" in rule.rule:
+            print("Entrei no finish")
+            return self.post_install()
 
-        parser = reqparse.RequestParser()
-        parser.add_argument("text")
-        parser.add_argument("channel_id")
-        parser.add_argument("response_url")
-        parser.add_argument("user_name")
-        parser.add_argument("user_id")
-        args = parser.parse_args()
-
-        text = args["text"]
-        channel_id = args["channel_id"]
-        response_url = args["response_url"]
-        user_name = args["user_name"]
-        user_id = args["user_id"]
-
-        if text is not None and response_url is not None \
-                and channel_id is not None:
-            self.semanticizer.dispatch_msg(text, channel_id,
-                                           user_name, user_id)
         else:
-            return self._error_return_msg(), 404
+            self._set_semanticizer_language()
 
-        return self._success_return_msg(text), 200
+            parser = reqparse.RequestParser()
+            parser.add_argument("text")
+            parser.add_argument("channel_id")
+            parser.add_argument("response_url")
+            parser.add_argument("user_name")
+            parser.add_argument("user_id")
+            args = parser.parse_args()
+
+            text = args["text"]
+            channel_id = args["channel_id"]
+            response_url = args["response_url"]
+            user_name = args["user_name"]
+            user_id = args["user_id"]
+
+            if text is not None and response_url is not None \
+                    and channel_id is not None:
+                self.semanticizer.dispatch_msg(text, channel_id,
+                                               user_name, user_id)
+            else:
+                return self._error_return_msg(), 404
+
+            return self._success_return_msg(text), 200
 
     def _set_semanticizer_language(self):
 
@@ -71,7 +126,7 @@ class PersonalAssistant(Resource):
         return "I'm analyzing your phrase: '{}'.".format(text)
 
 
-api.add_resource(PersonalAssistant, "/assistente_pessoal", "/personal_assistant")
+api.add_resource(PersonalAssistant, "/assistente_pessoal", "/personal_assistant", "/begin_auth", "/finish_auth")
 port = int(os.environ.get('PORT', 5000))
 
 if __name__ == "__main__":
